1b92400c4ede9bc642ce9bb906ae860413b7ea96
Tetsuro Kurita <tkurita@mac.com>
2005-08-23 17:26:38 +0000

    PaletteWindowController を継承して、window 位置の保存機能だけを利用。ヘルプ表示機能

---
+++ SettingWindowController.h
@@ -0,0 +1,11 @@
+/* SettingWindowController */
+
+#import <Cocoa/Cocoa.h>
+#import "PaletteWindowController.h"
+
+@interface SettingWindowController : PaletteWindowController
+{
+}
+- (IBAction)showSettingHelp:(id)sender;
+
+@end
---
+++ SettingWindowController.m
@@ -0,0 +1,19 @@
+#import "SettingWindowController.h"
+
+@implementation SettingWindowController
+
+- (IBAction)showSettingHelp:(id)sender
+{
+	NSHelpManager *helpManager = [NSHelpManager sharedHelpManager];
+	NSDictionary *theDict = [[NSBundle mainBundle] infoDictionary];
+	NSString *bookName = [theDict objectForKey:@"CFBundleHelpBookName"];
+	
+	[helpManager openHelpAnchor:@"Setting" inBook:bookName];
+}
+
+- (void)awakeFromNib
+{
+	[self setFrameName:@"Setting"];
+}
+
+@end
