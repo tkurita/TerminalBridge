74bdc90c8515ec056750d6b25bed652ca4559f33
Tetsuro Kurita <tkurita@mac.com>
2005-10-30 13:59:09 +0000

    windowShouldClose で window を orderOut するようにした。MacOS X 10.4 との互換性のため

--- SettingWindowController.m
+++ SettingWindowController.m
@@ -11,6 +11,13 @@
 	[helpManager openHelpAnchor:@"Setting" inBook:bookName];
 }
 
+- (BOOL)windowShouldClose:(id)sender
+{
+	/* To support AppleScript Studio of MacOS X 10.4 */
+	[[self window] orderOut:self];
+	return NO;
+}
+
 - (void)awakeFromNib
 {
 	[[self window] center];
