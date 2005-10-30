f8737f674329b80a4e4efc71863c78137f4d7616
Tetsuro Kurita <tkurita@mac.com>
2005-10-30 13:59:59 +0000

    SmartActivate を独立したクラスにした。

--- SmartActivate.h
+++ SmartActivate.h
@@ -1,9 +1,15 @@
-
 #import <Cocoa/Cocoa.h>
 #include <ApplicationServices/ApplicationServices.h>
+#include <IOKit/IOCFBundle.h>
 
-@interface NSApplication (SmartActivate) 
+NSDictionary *getProcessInfo(id targetCreator, id targetName, id targetIdentifier);
 
--(BOOL)smartActivate:(NSString *)targetCreator;
+@interface SmartActivate : NSObject {
+
+}
+
++(BOOL)activateAppOfType:(NSString *)targetCreator processName:(NSString*)targetName identifier:(NSString*)targetIdentifier;
++(BOOL)activateAppOfType:(NSString *)targetCreator;
++(BOOL)activateAppOfName:(NSString *)targetName;
 
 @end
--- SmartActivate.m
+++ SmartActivate.m
@@ -1,25 +1,71 @@
 #import "SmartActivate.h"
 
-NSDictionary *getProcessInfoForCreator(NSString *targetCreator) {
+#define useLog 0
+
+//NSDictionary *getProcessInfo(NSString *targetCreator, NSString *targetName, NSString *targetIdentifier) {
+NSDictionary *getProcessInfo(id targetCreator, id targetName, id targetIdentifier) {
 	/* find an applicarion process specified by theSignature(creator type) from runnning process.
-		if target application can be found, get information of the process and return as a result
+	if target application can be found, get information of the process and return as a result
 	*/
-	OSErr err;
+#if useLog
+	NSLog(@"start getProcessInfo");
+#endif
+	NSMutableArray *keyList = [NSMutableArray arrayWithObjects:@"FileCreator",@"CFBundleIdentifier",@"CFBundleName",nil];
+
+	unsigned int firstKeyIndex;
+	
+	if (targetCreator != nil) {
+		firstKeyIndex = 0;
+	}
+	else if (targetIdentifier != nil) {
+		firstKeyIndex = 1;
+	}
+	else {
+		firstKeyIndex = 2;
+	}
+	
+	if (targetCreator == nil) targetCreator = [NSNull null];
+	if (targetName == nil) targetName = [NSNull null];
+	if (targetIdentifier == nil) targetIdentifier = [NSNull null];
+	NSMutableArray *valueList = [NSMutableArray arrayWithObjects:targetCreator,targetIdentifier,targetName,nil];
+	
+	NSString *dictKey = [keyList objectAtIndex:firstKeyIndex];
+	[keyList removeObjectAtIndex:firstKeyIndex];
+	NSString *targetValue = [valueList objectAtIndex:firstKeyIndex];
+	[valueList removeObjectAtIndex:firstKeyIndex];
+	
+	BOOL isFound = NO;
 	ProcessSerialNumber psn = {kNoProcess, kNoProcess};
 	NSDictionary *pDict;
-	NSString *fileCreator;
-	BOOL isFound = NO;
-	
-	err = GetNextProcess(&psn);
+
+	OSErr err = GetNextProcess(&psn);
 	while( err == noErr) {
 		pDict = (NSDictionary *)ProcessInformationCopyDictionary(&psn, kProcessDictionaryIncludeAllInformationMask);
-		fileCreator = [pDict objectForKey:@"FileCreator"];
-		if (fileCreator != nil) {
-			if ([fileCreator isEqualToString:targetCreator]){
+		NSString *dictValue = [pDict objectForKey:dictKey];
+#if useLog
+		NSLog([pDict description]);
+		NSLog(dictKey);
+		NSLog(dictValue);
+		NSLog(targetValue);
+#endif
+		if (dictValue != nil) {
+			if ([dictValue isEqualToString:targetValue]){
 				isFound = YES;
-				break;
+				for (int i=0; i < 2; i++) {
+					NSString *secondValue = [valueList objectAtIndex:i];
+					if ([secondValue isNotEqualTo: [NSNull null]]) {
+						dictValue = [pDict objectForKey:[keyList objectAtIndex:i]];
+						if (![dictValue isEqualToString:secondValue]) {
+							isFound = NO;
+							break;
+						}
+					}
+				}
+				
+				if (isFound) break;
 			}
 		}
+		
 		[pDict release];
 		err = GetNextProcess (&psn);
 	}
@@ -28,20 +74,33 @@ NSDictionary *getProcessInfoForCreator(NSString *targetCreator) {
 		return pDict;
 	}
 	else{
-		//printf("NULL will be retruned\n");
 		return nil;
 	}
 }
 
-@implementation NSApplication (SmartActivate)
+@implementation SmartActivate
 
--(BOOL)smartActivate:(NSString *)targetCreator {
-	//printf("start smartActivate\n");
-	
-	NSDictionary *pDict = getProcessInfoForCreator(targetCreator);
++(BOOL)activateAppOfName:(NSString *)targetName
+{
+	return [self activateAppOfType:nil processName:targetName identifier:nil];
+}
+
++(BOOL)activateAppOfType:(NSString *)targetCreator
+{
+	return [self activateAppOfType:targetCreator processName:nil identifier:nil];
+}
+
++(BOOL)activateAppOfType:(NSString *)targetCreator processName:(NSString*)targetName identifier:(NSString*)targetIdentifier {
+#if useLog	
+	NSLog(@"start activateAppOfType");
+#endif
+
+	NSDictionary *pDict = getProcessInfo(targetCreator,targetName,targetIdentifier);
 	if (pDict != nil) {
-		//NSLog(@"will activate");
-		//NSLog([pDict description]);
+#if useLog
+		NSLog(@"will activate");
+		NSLog([pDict description]);
+#endif
 		ProcessSerialNumber psn;
 		[[pDict objectForKey:@"PSN"] getValue:&psn];
 		SetFrontProcessWithOptions(&psn,kSetFrontProcessFrontWindowOnly);
