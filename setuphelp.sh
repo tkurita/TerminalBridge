fc91a6a6871b4cfc24225beb0b25c2c16601f1ac
Tetsuro Kurita <tkurita@mac.com>
2006-01-30 11:03:56 +0000

    Help ファイルをセットアップするシェルスクリプト

---
+++ setuphelp.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+TARGET_NAME="UnixScriptServer"
+sitetear_path='/usr/local/bin/sitetear'
+manual_folder='/Users/tkurita/Factories/Websites/scriptfactory/scriptfactory/ScriptGallery/TheOtherScripts/UnixScriptToolsFormi/manual'
+#iconPath="$manual_folder/TeXCompileServerIcon16.png"
+
+copyHelp() {
+	manual_path=$1;
+	helpdir=$2;
+	mkdir -p "$helpdir"
+	perl "$sitetear_path" "$manual_path" "$helpdir"
+	open -a 'Help Indexer' "$helpdir"
+	#cp "$iconPath" "$helpdir"
+}
+
+helpdir="${TARGET_NAME}Help"
+
+manual_page="index.html" 
+
+copyHelp "$manual_folder/$manual_page" "$helpdir"
+
+
